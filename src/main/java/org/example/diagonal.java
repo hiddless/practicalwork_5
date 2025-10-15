5cd086216dc72e123130f7c50cfb3222d909a4f9
hiddless <christophertheodorepayne@gmail.com>
2025-10-15 23:34:36 +0300

    prac work 5

--- src/main/java/org/example/diagonal.java
+++ src/main/java/org/example/diagonal.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class diagonal {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.print("size r x r:");
-        int r = scn.nextInt();
-        int[][] duru = new int[r][r];
+      Scanner scn = new Scanner(System.in);
 
-        for (int i=0; i<r; i++){
-            for (int j=0; j<r; j++){
-                if(i==j || i + j == r-1){
-                    duru[i][j] = r;
-                }else {
-                    duru[i][j] = 0;
+      System.out.print("n: ");
+      int n = scn.nextInt();
+
+      int[][] duru = new int[n][n];
+      int k = 1;
+
+      for (int i = 0; i < n; i++) {
+          for (int j = n - 1; j >= 0; j--) {
+              if (i + j >= n - 3 && i + j <= n - 1) {
+                        duru[i][j] = k++;
+                } else {
+                        duru[i][j] = 0;
+                }
                 }
             }
-        }
-        for (int i = 0; i < duru.length; i++) {
-            for (int j = 0; j < duru[i].length; j++) {
-                System.out.printf("%-3d ", duru[i][j]);
-            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.printf("%-3d ", duru[i][j]);
+                }
             System.out.println();
-        }
+            }
         scn.close();
     }
 }
--- src/main/java/org/example/triangles.java
+++ src/main/java/org/example/triangles.java
@@ -4,6 +4,28 @@ import java.util.Scanner;
 
 public class triangles {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("n:");
+        int n = scn.nextInt();
+
+        int[][] duru = new int[n][n];
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(i + j<=n-1){
+                    duru[i][j] = i + j;
+                }else {
+                    duru[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < duru.length; i++) {
+            for (int j = 0; j < duru[i].length; j++) {
+                System.out.printf("%-3d ", duru[i][j]);
+            }
+            System.out.println();
+        }
+
 
     }
 }
