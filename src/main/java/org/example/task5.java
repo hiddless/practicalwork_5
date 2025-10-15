fbceb763466aa4765804958db20d87e6560ccb29
hiddless <christophertheodorepayne@gmail.com>
2025-10-15 22:42:12 +0300

    prac work 5

--- src/main/java/org/example/task5.java
+++ src/main/java/org/example/task5.java
@@ -5,29 +5,24 @@ import java.util.Scanner;
 public class task5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        System.out.print("n: ");
+        int n = scn.nextInt();
 
-        System.out.print("rows: ");
-        int r = scn.nextInt();
+        int[][] duru = new int[n][n];
 
-        System.out.print("columns: ");
-        int c = scn.nextInt();
-
-        int[][] d = new int[r][c];
-        int num = 1;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                d[i][j] = num++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                duru[i][j] = n * j + (n - 1 - i);
             }
         }
-
-        for (int i = 0; i < d.length; i++) {
-            for (int j = 0; j < d[i].length; j++) {
-                System.out.printf("%-3d ", d[i][j]);
+        for (int i = 0; i < duru.length; i++) {
+            for (int j = 0; j < duru[i].length; j++) {
+                System.out.printf("%-3d ", duru[i][j]);
             }
             System.out.println();
         }
 
         scn.close();
+
     }
 }
